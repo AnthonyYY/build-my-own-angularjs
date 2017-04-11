330a3f8847d3323988574e36e1f8d1f031c3cd9c
AnthonyYY <yjh2332@gmail.com>
2017-04-11 08:17:43 +0800

    chapter2

--- src/scope.js
+++ src/scope.js
@@ -4,21 +4,26 @@ var _ = require('lodash');
 
 function Scope(){
   this.$$watchers = [];
-};
+}
 
 Scope.prototype.$watch = function(watchFn,listenerFn){
   var watcher = {
     watchFn: watchFn,
     listenerFn: listenerFn
-  }
+  };
   this.$$watchers.push(watcher);
-}
+};
 
 Scope.prototype.$digest = function(){
   var self = this;
+  var newValue,oldValue;
   _.forEach(this.$$watchers,function(watcher){
-    watcher.watchFn(self);
-    watcher.listenerFn();
+    newValue = watcher.watchFn(self);
+    oldValue = watcher.last;
+    if(newValue != oldValue){
+      watcher.last = newValue;
+      watcher.listenerFn(newValue,oldValue,self);
+    }
   });
 };
 
--- test/scope_spec.js
+++ test/scope_spec.js
@@ -14,22 +14,46 @@ describe('Scope',function(){
     });
 
     it('calls the listener function of a watch on first $digest', function(){
-      var watchFn = function(){ return 'cat' };
+      var watchFn = function(){ return 'cat'; };
       var listenerFn = jasmine.createSpy();
       scope.$watch(watchFn,listenerFn);
 
       scope.$digest();
-
       expect(listenerFn).toHaveBeenCalled();
     });
 
     it('calls the watch function with the scope as the argument', function(){
       var watchFn = jasmine.createSpy();
       var listenerFn = function(){};
-
       scope.$watch(watchFn,listenerFn);
 
-      expect(watchFn).toHaveBeenCalledWith(scope)
+      scope.$digest();
+
+      expect(watchFn).toHaveBeenCalledWith(scope);
+    });
+
+    it('calls the listener function when whe watched value changes',function(){
+      scope.someValue = 'a';
+      scope.counter = 0;
+
+      scope.$watch(
+        function(scope){ return scope.someValue; },
+        function(newValue,oldValue,scope){ scope.counter++; }
+      );
+
+      expect(scope.counter).toBe(0);
+
+      scope.$digest();
+      expect(scope.counter).toBe(1);
+
+      scope.$digest();
+      expect(scope.counter).toBe(1);
+
+      scope.someValue = 'b';
+      expect(scope.counter).toBe(1);
+
+      scope.$digest();
+      expect(scope.counter).toBe(2);
     })
   });
 });
